608fdb6377e0d8c545d1ab5d56346de951dee01c
Glaiza Cano <glaizaccano@gmail.com>
2024-04-11 11:26:55 +1000

    ✨ Intialise directory for practice assignments
    Add index.html and script.js files for 'Values and Variables' practice assignment

---
+++ 01-js-fundamentals-part-1/practice-assignments/section-2/01-values-and-variables/script.js
@@ -0,0 +1,7 @@
+let country = 'Australia';
+let continent = 'Oceania';
+let population = '26010000';
+
+console.log('Country: ' + country);
+console.log('Continent: ' + continent);
+console.log('Population: ' + population);
