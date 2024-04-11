4207aeeccf8b5b0deb4a6ec0e761ac4068b3157b
Glaiza Cano <glaizaccano@gmail.com>
2024-04-11 18:13:18 +1000

    🚧 Update coding exercise files for 'Data Types'
    Update script.js file

--- 01-js-fundamentals-part-1/script.js
+++ 01-js-fundamentals-part-1/script.js
@@ -1,4 +1,4 @@
-let js = 'amazing';
+/* let js = 'amazing';
 // if (js === 'amazing') alert('JavaScript is FUN!');
 
 // 40 + 8 + 23 - 10;
@@ -22,4 +22,24 @@ let PI = 3.1415;
 let myFirstJob = 'Coder';
 let myCurrentJob = 'Teacher';
 
-console.log(myFirstJob);
+console.log(myFirstJob);*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
