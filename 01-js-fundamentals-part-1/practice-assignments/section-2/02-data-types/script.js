a6b15e8fd9a3fa96fcc0df2ab3a620c3abaa5ce1
Glaiza Cano <glaizaccano@gmail.com>
2024-04-11 18:15:28 +1000

    ✨ Add practice assignment for 'Data Types'

---
+++ 01-js-fundamentals-part-1/practice-assignments/section-2/02-data-types/script.js
@@ -0,0 +1,15 @@
+let country = 'Australia';
+let continent = 'Oceania';
+let population = '26010000';
+
+// Data Types
+
+/* 1. Declare a variable called isIsland and set its value according to your country. The variable should hold a Boolean value. Also declare a variable language, but don't assign it any value yet. */
+let isIsland = false;
+let language;
+
+/* 2. Log the types of isIsland, population, country and language to the console. */
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
