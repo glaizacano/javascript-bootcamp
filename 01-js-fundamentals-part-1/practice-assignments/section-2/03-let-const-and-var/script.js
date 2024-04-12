44c32e83f1d78fdbba61f7203597ff544d3bca80
Glaiza Cano <glaizaccano@gmail.com>
2024-04-12 12:43:48 +1000

    ✨ Add practice assignment for 'let, const and var'

---
+++ 01-js-fundamentals-part-1/practice-assignments/section-2/03-let-const-and-var/script.js
@@ -0,0 +1,14 @@
+// let, const and var
+
+/* 2. Think about which variables should be const variables (which values will never change, and which might change?). Then, change these variables to const. */
+const country = 'Australia';
+const continent = 'Oceania';
+let population = '26010000';
+
+const isIsland = false;
+
+/* 1. Set the value of language to the language spoken where you live (some countries have multiple languages, but just choose one). */
+let language = 'English';
+
+/* 3. Try to change one of the changed variables now, and observe what happens. */
+isIsland = true;
