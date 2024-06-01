29f89c55a82ffce5468e4ca662793634429b9366
Glaiza Cano <glaizaccano@gmail.com>
2024-06-01 15:59:11 +1000

    ✨ Add practice assignment for 'String and Template Literals'

---
+++ 01-js-fundamentals-part-1/practice-assignments/section-2/05-strings-and-template-literals/script.js
@@ -0,0 +1,38 @@
+const country = 'Australia';
+const continent = 'Oceania';
+let population = '26010000';
+
+const isIsland = false;
+let language = 'English';
+
+// isIsland = true;
+
+// Basic Operators
+
+/* 1. If your country split in half, and each half would contain half the population, then how many people would live in each half? */
+console.log('Half of ' + country + "'s population: " + population / 2);
+
+/* 2. Increase the population of your country by 1 and log the result to the console.
+population++; */
+console.log(population);
+
+/* 3. Finland has a population of 6 million. Does your country have more people than Finland? */
+console.log(population > 6000000);
+
+/* 4. The average population of a country is 33 million people. Does you country have less people than the average country? */
+console.log(population < 33000000);
+
+/* 5. Based on the variables you created, create a new variable description which contains a string with this format: 'Portugal is in Europe, and its 11 million people speak portuguese'. */
+// description =
+//   country +
+//   ' is in ' +
+//   continent +
+//   ', and its ' +
+//   population +
+//   ' people speak ' +
+//   language +
+//   '.';
+// console.log(description);
+
+const description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
+console.log(description);
