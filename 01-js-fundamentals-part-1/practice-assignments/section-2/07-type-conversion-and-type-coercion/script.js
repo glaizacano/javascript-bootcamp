d90d204f353264a04cd3c441233973aeece80702
Glaiza Cano <glaizaccano@gmail.com>
2024-06-02 00:44:39 +1000

    ✨ Add practice assignment for 'Type Conversion and Coercion'

---
+++ 01-js-fundamentals-part-1/practice-assignments/section-2/07-type-conversion-and-type-coercion/script.js
@@ -0,0 +1,8 @@
+// Type Conversion and Coercion
+
+// 1. Predict the result of these 5 operations without executing them:
+console.log('9' - '5'); // -> 4
+console.log('19' - '13' + '17'); // -> '617'
+console.log('19' - '13' + 17); // -> 23
+console.log('123' < 57); // -> false
+console.log(5 + 6 + '4' + 9 - 4 - 2); // -> 1143
