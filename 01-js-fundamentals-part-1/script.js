e9b5101a11d2c51fb0ccf26eb691e645e1226816
Glaiza Cano <glaizaccano@gmail.com>
2024-06-01 15:04:55 +1000

    🚧 Update coding exercise files for 'Operator Precedence'

--- 01-js-fundamentals-part-1/script.js
+++ 01-js-fundamentals-part-1/script.js
@@ -55,7 +55,7 @@ job = 'teacher';
 
 // incorrect way of creating variables
 lastName = 'Schmedtmann';
-console.log(lastName); */
+console.log(lastName);
 
 // Math operators
 const now = 2037;
@@ -84,4 +84,23 @@ console.log(ageJonas > ageSarah); // >, <. >=, <=
 console.log(ageSarah >= 18);
 
 const isFullAge = ageSarah >= 18;
+console.log(now - 1991 > now - 2018); */
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
 console.log(now - 1991 > now - 2018);
+
+// console.log(25 - 10 - 5);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10, y = 10
+console.log(x, y);
+
+console.log(ageJonas);
+console.log(ageSarah);
+
+const averageAge = (ageJonas + ageJonas) / 2;
+console.log(ageJonas, ageSarah, averageAge);
