ccb3c736fefb6b4a30a4ddd8fcb39784d25fd58b
Glaiza Cano <glaizaccano@gmail.com>
2024-06-04 16:52:05 +1000

    🚧 Update coding exercise files for 'Logical Operators'

--- 01-js-fundamentals-part-1/script.js
+++ 01-js-fundamentals-part-1/script.js
@@ -199,23 +199,46 @@ console.log(now - 1991 > now - 2018); */
 // }
 
 // Equality Operators: == vs. ===
-const age = '18';
-if (age === 18) console.log('You just became an adult :D (strict)');
-if (age == 18) console.log('You just became an adult :D (loose)');
+// const age = '18';
+// if (age === 18) console.log('You just became an adult :D (strict)');
+// if (age == 18) console.log('You just became an adult :D (loose)');
 
-const favourite = Number(prompt("What's your favourite number?"));
-console.log(favourite);
-console.log(typeof favourite);
+// const favourite = Number(prompt("What's your favourite number?"));
+// console.log(favourite);
+// console.log(typeof favourite);
 
-if (favourite === 23) {
-  // '23' == 23
-  console.log('Cool! 23 is an amazing number!');
-} else if (favourite === 7) {
-  console.log('7 is also a cool number');
-} else if (favourite === 9) {
-  console.log('9 is also a cool number');
+// if (favourite === 23) {
+//   // '23' == 23
+//   console.log('Cool! 23 is an amazing number!');
+// } else if (favourite === 7) {
+//   console.log('7 is also a cool number');
+// } else if (favourite === 9) {
+//   console.log('9 is also a cool number');
+// } else {
+//   console.log('Number is not 23 or 7 or 9');
+// }
+
+// if (favourite !== 23) console.log('Why not 23?');
+
+// Logical Operators
+const hasDriverLicense = true; // A
+const hasGoodVision = true; // B
+
+console.log(hasDriverLicense && hasGoodVision); // -> false
+console.log(hasDriverLicense || hasGoodVision); // -> true
+console.log(!hasDriverLicense); // -> false
+
+// if (hasDriverLicense && hasGoodVision) {
+//   console.log('Sarah is able to drive!');
+// } else {
+//   console.log('Someone else should drive...');
+// }
+
+const isTired = false; // C
+console.log(hasDriverLicense && hasGoodVision && isTired);
+
+if (hasDriverLicense && hasGoodVision && !isTired) {
+  console.log('Sarah is able to drive!');
 } else {
-  console.log('Number is not 23 or 7 or 9');
+  console.log('Someone else should drive...');
 }
-
-if (favourite !== 23) console.log('Why not 23?');
