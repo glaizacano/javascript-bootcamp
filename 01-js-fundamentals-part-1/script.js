c9faae5e85a5684a7be6551078f89ec901cfb163
Glaiza Cano <glaizaccano@gmail.com>
2024-06-05 20:13:22 +1000

    🚧 Update coding exercise files for 'The Conditional (Ternary) Operator'

--- 01-js-fundamentals-part-1/script.js
+++ 01-js-fundamentals-part-1/script.js
@@ -288,15 +288,35 @@ console.log(now - 1991 > now - 2018); */
 // Statements and Expressions
 
 // Expressions
-3 + 4;
-1991;
-true && false && !false;
+// 3 + 4;
+// 1991;
+// true && false && !false;
 
 // Statements
-if (23 > 10) {
-  const str = '23 is bigger';
-}
+// if (23 > 10) {
+//   const str = '23 is bigger';
+// }
 
-const me = 'Jonas';
-// Template literal that uses an expression
-console.log(`I'm ${2037 - 1991} years old ${me}`);
+// const me = 'Jonas';
+// Expression used in a template literal
+// console.log(`I'm ${2037 - 1991} years old ${me}`);
+
+// The Conditional (Ternary) Operator
+const age = 23;
+age >= 18
+  ? console.log('I like to drink wine 🍷')
+  : console.log('I like to drink water 💧');
+
+const drink = age >= 18 ? 'wine 🍷' : 'water 💧';
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+  drink2 = 'wine 🍷';
+} else {
+  drink2 = 'water 💧';
+}
+console.log(drink2);
+
+// Expression used in a template literal
+console.log(`I like to drink ${age >= 18 ? 'wine 🍷' : 'water 💧'}`);
