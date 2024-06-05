6a12e59fd6b52df700f584dbe3eb2023e6f8f0e4
Glaiza Cano <glaizaccano@gmail.com>
2024-06-05 19:53:45 +1000

    🚧 Update coding exercise files for 'Statements and Expressions'

--- 01-js-fundamentals-part-1/script.js
+++ 01-js-fundamentals-part-1/script.js
@@ -244,31 +244,31 @@ console.log(now - 1991 > now - 2018); */
 // }
 
 // The switch Statement
-const day = 'sunday';
+// const day = 'sunday';
 
-switch (day) {
-  case 'monday': // day === monday
-    console.log('Plan course structure');
-    console.log('Go to coding meetup');
-    break;
-  case 'tuesday': // day === tuesday
-    console.log('Prepare theory videos');
-    break;
-  case 'wednesday': // day === wednesday
-  case 'thursday': // day === thursday
-    console.log('Write code examples');
-    break;
-  case 'friday': // day === friday
-    console.log('Record videos');
-    break;
-  case 'saturday': // day === saturday
-  case 'sunday': // day === sunday
-    console.log('Enjoy the weekend :D');
-    break;
-  default:
-    console.log('Not a valid day!');
-    break;
-}
+// switch (day) {
+//   case 'monday': // day === monday
+//     console.log('Plan course structure');
+//     console.log('Go to coding meetup');
+//     break;
+//   case 'tuesday': // day === tuesday
+//     console.log('Prepare theory videos');
+//     break;
+//   case 'wednesday': // day === wednesday
+//   case 'thursday': // day === thursday
+//     console.log('Write code examples');
+//     break;
+//   case 'friday': // day === friday
+//     console.log('Record videos');
+//     break;
+//   case 'saturday': // day === saturday
+//   case 'sunday': // day === sunday
+//     console.log('Enjoy the weekend :D');
+//     break;
+//   default:
+//     console.log('Not a valid day!');
+//     break;
+// }
 
 // if (day === 'monday') {
 //   console.log('Plan course structure');
@@ -284,3 +284,19 @@ switch (day) {
 // } else {
 //   console.log('Not a valid day!');
 // }
+
+// Statements and Expressions
+
+// Expressions
+3 + 4;
+1991;
+true && false && !false;
+
+// Statements
+if (23 > 10) {
+  const str = '23 is bigger';
+}
+
+const me = 'Jonas';
+// Template literal that uses an expression
+console.log(`I'm ${2037 - 1991} years old ${me}`);
